005dd4e18fef38f41a0ae29fe076e9d615b63d40
Gergely <gergely.kantor@t-systems.com>
2024-06-17 09:55:36 +0200

    custom kw py file deleted

--- resources/custom_kw_defs.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_most_recent_file(directory, pattern):
-    files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(pattern)]
-    most_recent_file = max(files, key=os.path.getctime)
-    return most_recent_file
